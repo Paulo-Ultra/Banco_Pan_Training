01136f98028519b8d42754b8cfb67df228628a5b
Paulo Ricardo Freire Ultra <84856841+Paulo-Ultra@users.noreply.github.com>
2021-10-19 10:30:23 -0300

    Delete crud directory

--- crud/src/main/java/com/example/demo/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.ClienteModel;
-
-public interface ClienteRepository extends JpaRepository<ClienteModel, Long>{
-
-}
