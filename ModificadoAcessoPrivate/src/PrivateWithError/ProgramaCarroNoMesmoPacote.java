da1f8a42bdcb75458d3dfe3feeb865c5bf5f689a
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2021-10-19 11:03:43 -0300

    Acess Private Modifiers

---
+++ ModificadoAcessoPrivate/src/PrivateWithError/ProgramaCarroNoMesmoPacote.java
@@ -0,0 +1,12 @@
+package PrivateWithError;
+
+public class ProgramaCarroNoMesmoPacote {
+
+	public static void main(String[] args) {
+		//instancia o objeto
+		Carro c = new Carro();
+		c.nome = "Ferrari";
+		c.exibeVelocidade();
+		}
+
+}
