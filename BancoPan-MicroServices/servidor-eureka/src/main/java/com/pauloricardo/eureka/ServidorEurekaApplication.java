ff3abd7a9faa3f8b43873fc8420b1b007c79d9f6
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2021-10-27 22:57:27 -0300

    EurekaServer

---
+++ BancoPan-MicroServices/servidor-eureka/src/main/java/com/pauloricardo/eureka/ServidorEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.pauloricardo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class ServidorEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorEurekaApplication.class, args);
+	}
+
+}
---
+++ BancoPan-MicroServices/servidor-eureka/src/test/java/com/pauloricardo/eureka/ServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloricardo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
