c6d495d34431be6d71ff38f8707ed4110e5c3d3e
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2021-10-28 22:40:31 -0300

    Parsing JSON

---
+++ json/src/main/java/com/pauloricardo/json/JsonApplication.java
@@ -0,0 +1,38 @@
+package com.pauloricardo.json;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.json.simple.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JsonApplication {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		SpringApplication.run(JsonApplication.class, args);
+		
+		JSONObject jsonzinho = new JSONObject();
+		
+		jsonzinho.put("Nome", "Julio");
+		jsonzinho.put("Sobrenome", "Smith");
+		jsonzinho.put("idade", 25);
+		jsonzinho.put("Nome", "Julio");
+		jsonzinho.put("Sobrenome", "Smith");
+		jsonzinho.put("idade", 25);
+
+		System.out.println(jsonzinho);
+		
+		//Writing JSON to file: "JSONExample.json" in cwd
+		PrintWriter salvar =  new PrintWriter("./src/main/java/teste2.json");
+		salvar.write(jsonzinho.toJSONString());
+		
+		salvar.flush();
+		salvar.close();
+		
+	}
+	
+
+}
---
+++ json/src/main/java/com/pauloricardo/json/Leitura.java
@@ -0,0 +1,33 @@
+package com.pauloricardo.json;
+
+import java.io.FileNotFoundException;
+
+// Java program to read JSON from a file
+
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Leitura {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+
+		Object obj = new JSONParser().parse(new FileReader("./src/main/java/teste2.json"));
+
+		JSONObject jsonzinho = (JSONObject) obj;
+
+		// get nome
+		String nominho = (String) jsonzinho.get("Nome");
+		String sobrenominho = (String) jsonzinho.get("Sobrenome");
+		long idade = (long) jsonzinho.get("idade");
+		
+		System.out.println(nominho);
+		System.out.println(sobrenominho);
+		System.out.println(idade);
+		
+	}
+}
